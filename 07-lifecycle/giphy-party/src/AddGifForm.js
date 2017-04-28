3b57419ded6ae9e9cdbed251dcb62d6e18e82134
Elie Schoppik <eschoppik@gmail.com>
2017-04-28 15:01:27 -0700

    giphy party complete

--- 07-lifecycle/giphy-party/src/AddGifForm.js
+++ 07-lifecycle/giphy-party/src/AddGifForm.js
@@ -8,6 +8,15 @@ export default class AddGifForm extends Component {
     }
     this.getGifs = this.getGifs.bind(this)
     this.handleChange = this.handleChange.bind(this)
+    this.resetGifs = this.resetGifs.bind(this)
+  }
+  componentDidMount() {
+    let randomTerms = ["happy", "funny", "baby", "silly", "party", "dance", "sing", "music", "jump", "run"];
+    fetch(`https://api.giphy.com/v1/gifs/search?q=${randomTerms[Math.floor(Math.random()*randomTerms.length)]}&api_key=dc6zaTOxFJmzC`)
+      .then(r => r.json())
+      .then(r => {
+        this.props.handleSubmit(r)
+      })
   }
   handleChange(e){
     this.setState({
@@ -16,16 +25,28 @@ export default class AddGifForm extends Component {
   }
   getGifs(e){
     e.preventDefault()
+    fetch(`https://api.giphy.com/v1/gifs/search?q=${this.state.searchTerm}&api_key=dc6zaTOxFJmzC`)
+      .then(r => r.json())
+      .then(r => {
+        this.props.handleSubmit(r)
+      })
+    this.setState({
+      searchTerm: ''
+    })
+  }
+  resetGifs(){
+    this.props.clearGifs();
   }
   render(){
     return(
         <div>
-          <form onSubmit={this.getGifs}>
-            <input type="text" value={this.state.searchTerm} name="searchTerm" onChange={this.handleChange}/>
-            <input type="submit" className="btn btn-primary" value="Search Giphy"/>
-          </form>
-
-          <button className="btn btn-danger" onClick={this.resetGifs}>Remove Images</button>
+          <div className="row">
+            <form className="form-inline col-sm-offset-3 col-sm-4" onSubmit={this.getGifs}>
+              <input type="text" className="form-control" value={this.state.searchTerm} name="searchTerm" onChange={this.handleChange}/>
+              <input type="submit" className="btn btn-primary" value="Search Giphy"/>
+            </form>
+            <button className="col-sm-2 btn btn-danger" onClick={this.resetGifs}>Remove Images</button>
+          </div>
         </div>
       )
   }
--- 07-lifecycle/giphy-party/src/GifList.js
+++ 07-lifecycle/giphy-party/src/GifList.js
@@ -7,15 +7,29 @@ export default class GifList extends Component {
     this.state = {
       gifs: []
     }
+    this.updateList = this.updateList.bind(this)
+    this.clearList = this.clearList.bind(this)
+  }
+  updateList(response){
+    this.setState({
+      gifs: this.state.gifs.concat(response.data[Math.floor(Math.random()*response.data.length)])
+    })
+  }
+  clearList(){
+    this.setState({
+      gifs: []
+    })
   }
   render(){
-    let gifs = this.state.gifs.map(val => {
-      return <div>Hello!</div>
+    let gifs = this.state.gifs.map((val,idx) => {
+      return <div className="col-sm-3" key={idx}>
+          <iframe src={val.embed_url}/>
+        </div>
     })
     return(
       <div>
-        <h1>GIPHY PARTY</h1>
-        <AddGifForm/>
+        <h1 className="text-center">GIPHY PARTY</h1>
+        <AddGifForm handleSubmit={this.updateList} clearGifs={this.clearList}/>
         {gifs}
       </div>
     )
