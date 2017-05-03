59ebfd2fbe2578ced1187e963900b99d3f1f9b96
Tim Garcia <tim.m.garcia@gmail.com>
2017-05-02 20:36:34 -0700

    Random boxes solution

---
+++ 02-props-state-component-architecture/state/src/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import Box from './Box';
+import './App.css';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    var boxes = [];
+    const numBoxes = 24;
+    for (var i = 0; i < numBoxes; i++) {
+      let randomIndex = Math.floor(Math.random() * this.props.allColors.length);
+      let randomColor = this.props.allColors[randomIndex];
+      boxes.push({
+        id: i,
+        color: randomColor
+      });
+    }
+    this.state = {boxes};
+
+    setInterval(() => {
+      const newBoxes = this.state.boxes.slice();
+      const randomBoxIndex = Math.floor(Math.random() * newBoxes.length);
+      const colorIndex = Math.floor(Math.random() * this.props.allColors.length);
+      const randomColor = this.props.allColors[colorIndex];
+      newBoxes[randomBoxIndex].color = randomColor;
+      this.setState({
+        boxes: newBoxes
+      })
+    }, 300);
+  }
+
+  render() {
+    const boxes = this.state.boxes.map(b =>
+      <Box key={b.id} color={b.color} />
+    );
+    return (
+      <div className="App">
+        {boxes}
+      </div>
+    );
+  }
+}
+
+App.defaultProps = {
+  allColors: ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond",
+              "Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate",
+              "Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod",
+              "DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange",
+              "DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey",
+              "DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue",
+              "FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod",
+              "Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki",
+              "Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan",
+              "LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon",
+              "LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow",
+              "Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid",
+              "MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise",
+              "MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy",
+              "OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen",
+              "PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue",
+              "Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen",
+              "SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen",
+              "SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke",
+              "Yellow","YellowGreen"]
+}
