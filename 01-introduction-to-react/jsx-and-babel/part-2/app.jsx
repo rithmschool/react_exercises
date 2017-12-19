542e99f8c19fd3605e44eda66d820f0930c95b32
Matt Lane <lane.matthew@gmail.com>
2017-12-19 09:37:33 -0800

    reorganizing solutions

--- 01-introduction-to-react/jsx-and-babel/part-2/app.jsx
+++ 01-introduction-to-react/jsx-and-babel/part-2/app.jsx
@@ -1,23 +1,11 @@
-class Person extends React.Component {
-  
+class Tweet extends React.Component {
   render() {
-    let drinkText = this.props.age >= 21 ?
-      "Have a drink!" :
-      "You must be 21.";
-
-    let hobbies = this.props.hobbies.map((hobby,i) => <li key={i}>{hobby}</li>) 
-
     return (
-      <div>
-        <p>Learn some information about this person:</p>
-        <ul>
-          <li>Name: {this.props.name.slice(0,6)}</li>
-          <li>Age: {this.props.age}</li>
-          <ul>Hobbies
-            {hobbies}
-          </ul>
-        </ul>
-        <h3>{drinkText}</h3>
+      <div className="tweet">
+        <span>{this.props.name}</span>
+        <span className="username">@{this.props.username}</span>
+        <span className="date">{this.props.date}</span>
+        <p>{this.props.message}</p>
       </div>
     );
   }
@@ -27,25 +15,23 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Person 
-          name="Homer"
-          age={38}
-          hobbies={["bowling","watching tv","drinking beer"]}
+        <Tweet 
+          name="Matt Lane"
+          username="mmmaaatttttt"
+          date={new Date().toDateString()}
+          message="This app will disrupt everything!!"
         />
-        <Person
-          name="Marge"
-          age={34}
-          hobbies={["painting","gambling"]}
+        <Tweet 
+          name="Elie Schoppik"
+          username="eschoppik"
+          date={new Date().toDateString()}
+          message="#Ilovehashtags"
         />
-        <Person
-          name="Bart"
-          age={10}
-          hobbies={["skateboarding","making prank calls"]}
-        />
-        <Person
-          name="Lisa"
-          age={8}
-          hobbies={["reading","saxophone","eating vegetables"]}
+        <Tweet 
+          name="Tim Garcia"
+          username="TimGarcia0"
+          date={new Date().toDateString()}
+          message="Follow me on Twitter!"
         />
       </div>
     )
---
+++ 01-introduction-to-react/jsx-and-babel/part-3/app.jsx
@@ -0,0 +1,56 @@
+class Person extends React.Component {
+  
+  render() {
+    let drinkText = this.props.age >= 21 ?
+      "Have a drink!" :
+      "You must be 21.";
+
+    let hobbies = this.props.hobbies.map((hobby,i) => <li key={i}>{hobby}</li>) 
+
+    return (
+      <div>
+        <p>Learn some information about this person:</p>
+        <ul>
+          <li>Name: {this.props.name.slice(0,6)}</li>
+          <li>Age: {this.props.age}</li>
+          <ul>Hobbies
+            {hobbies}
+          </ul>
+        </ul>
+        <h3>{drinkText}</h3>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Person 
+          name="Homer"
+          age={38}
+          hobbies={["bowling","watching tv","drinking beer"]}
+        />
+        <Person
+          name="Marge"
+          age={34}
+          hobbies={["painting","gambling"]}
+        />
+        <Person
+          name="Bart"
+          age={10}
+          hobbies={["skateboarding","making prank calls"]}
+        />
+        <Person
+          name="Lisa"
+          age={8}
+          hobbies={["reading","saxophone","eating vegetables"]}
+        />
+      </div>
+    )
+  }
+}
+
+// lets put this on the DOM
+ReactDOM.render(<App/>, document.getElementById("app"));
