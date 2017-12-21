91136631e541e54ca3dded5f4eca733dc9335952
Trevor Hoy <trevorphoy@gmail.com>
2017-12-20 20:03:16 -0800

    finished part2 styling

--- 01-introduction-to-react/jsx-and-babel/app.jsx
+++ 01-introduction-to-react/jsx-and-babel/app.jsx
@@ -40,63 +40,16 @@
 
 // PART 2
 
-// class Tweet extends React.Component {
-// 	render() {
-// 		return (
-// 			<div>
-// 				<p className='primary-text'>{this.props.username}</p>
-// 				<p>{this.props.name}</p>
-// 				<p>{this.props.date}</p>
-// 				<p>{this.props.message}</p>
-// 			</div>
-// 		)
-// 	}
-// }
-
-// class App extends React.Component {
-// 	render() {
-// 		var style = {color: 'red'};
-// 		return (
-// 			<div>
-// 				<p className="primary-text">This should be large blue text</p>
-// 				<Tweet username='thoy19' name='Trevor' date='12/20/2017' message='This is my first react tweet :))'/>
-// 				<Tweet username='thoy19' name='Trevor' date='12/20/2017' message='This is my first react tweet :))'/>
-// 				<Tweet username='thoy19' name='Trevor' date='12/20/2017' message='This is my first react tweet :))'/>
-// 			</div>
-// 		)
-// 	}
-// }
-
-
-// PART 3
-
-class Person extends React.Component {
+class Tweet extends React.Component {
 	render() {
-			var name = this.props.name;
-			var age = this.props.age;
-			var hobbies = this.props.hobbies;
-			var canDrink = 'have a drink!'
-			var cannotDrink = 'you must be 21'
-
 		return (
 			<div>
-			<p>Learn some information about this person!</p>
-			{ name.length > 8 ? 
-				<h2>{ name.slice(0,6) }</h2>
-				:
-				<h2>{ name }</h2>
-			}
-			{age >= 21 ? 
-				<h3>{ canDrink }</h3>
-				:
-				<h3>{ cannotDrink }</h3>
-			}
-			<p>Your hobbies:</p>
-			<ul>
-				{ hobbies.map(hobby => {
-					return <li>{ hobby }</li>;
-				})}
-			</ul>
+				<div className='message'>
+					<p>@{this.props.username}</p>
+					<p>{this.props.name}</p>
+					<p>{this.props.date}</p>
+					<p>{this.props.message}</p>
+				</div>
 			</div>
 		)
 	}
@@ -104,13 +57,64 @@ class Person extends React.Component {
 
 class App extends React.Component {
 	render() {
+		var style = {color: 'red'};
 		return (
 			<div>
-				<Person name='Samwellbaggins' age='21' hobbies={['adventures','rings','frodo','eating','frodo']}/>
+				<p className="primary-text">This should be large blue text</p>
+				<Tweet username='thoy19' name='Trevor' date='12/20/2017' message='This is my first react tweet :))'/>
+				<Tweet username='thoy19' name='Trevor' date='12/20/2017' message='This is my first react tweet :))'/>
+				<Tweet username='thoy19' name='Trevor' date='12/20/2017' message='This is my first react tweet :))'/>
 			</div>
 		)
 	}
 }
+
+
+// PART 3
+
+// class Person extends React.Component {
+// 	render() {
+// 			var name = this.props.name;
+// 			var age = this.props.age;
+// 			var hobbies = this.props.hobbies;
+// 			var canDrink = 'have a drink!'
+// 			var cannotDrink = 'you must be 21'
+
+// 		return (
+// 			<div>
+// 			<p>Learn some information about this person!</p>
+// 			{ name.length > 8 ? 
+// 				<h2>{ name.slice(0,6) }</h2>
+// 				:
+// 				<h2>{ name }</h2>
+// 			}
+// 			{age >= 21 ? 
+// 				<h3>{ canDrink }</h3>
+// 				:
+// 				<h3>{ cannotDrink }</h3>
+// 			}
+// 			<p>Your hobbies:</p>
+// 			<ul>
+// 				{ hobbies.map(hobby => {
+// 					return <li>{ hobby }</li>;
+// 				})}
+// 			</ul>
+// 			</div>
+// 		)
+// 	}
+// }
+
+// class App extends React.Component {
+// 	render() {
+// 		return (
+// 			<div>
+// 				<Person name='Samwellbaggins' age='21' hobbies={['adventures','rings','frodo','eating','eating more']}/>
+// 				<Person name='Anotherperson' age='21' hobbies={['adventures','rings','frodo','eating','frodo']}/>
+// 				<Person name='Legolastheelf' age='21' hobbies={['bow','arrows','dwarfs','ring']}/>
+// 			</div>
+// 		)
+// 	}
+// }
 
 
 
