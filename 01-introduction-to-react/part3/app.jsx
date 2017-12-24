df9126f5b175f15b03c094ffe3b54fed5bffdfd0
Violeta Simovska <vikisimovska27@gmail.com>
2017-12-23 18:41:27 -0800

    adding solution for part3

--- 01-introduction-to-react/part3/app.jsx
+++ 01-introduction-to-react/part3/app.jsx
@@ -1,15 +1,53 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <link rel="stylesheet" type="text/css" href="app.css">
-  <title>React + Babel</title>
-</head>
-<body>
-  <div id="app"></div>
-  <script crossorigin src="https://unpkg.com/react@16/umd/react.development.js"></script>
-  <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
-  <script src="https://unpkg.com/babel-standalone@6/babel.js"></script>
-  <script type="text/babel" src="app.jsx"></script>
-</body>
-</html>
+class Person extends React.Component{
+
+	render(){
+        let message;
+        if(this.props.age > 21){
+        	message = "Have a drink!"
+        }else{
+            message = "You must be 21!"
+        }
+
+        let name;
+        if(this.props.name.length > 8){
+            name = this.props.name.slice(0,6);
+        }else{
+        	name = this.props.name;
+        }
+
+       var list;
+       var hobbieArr = [];
+       var hobbies = this.props.hobbies;
+       for (let i=0; i < hobbies.length; i++){
+       	        hobbieArr.push (<li key={i}>{hobbies[i]}</li>);
+       }
+
+		return(
+
+			<div>
+	             <h2>Learn some information about {name}:</h2>
+	             <p>{message}</p>
+	             <h4>Favorite Hobies:</h4>
+	             <ul>{hobbieArr}</ul>
+	        </div> 
+
+			);
+	}
+}
+
+class App extends React.Component{
+	render(){
+		return (
+             <div id="inner">
+                 <img src = "http://www.animated-gifs.eu/category_kitchen/kitchen-beer/0057.gif" alt = "drinking gif" />
+			     <Person name = "Annnnnnnnnnnn" age = {23} hobbies = {["Swimming", "Running", "Fishing"]} />
+			     <Person name = "Mike" age = {19} hobbies = {["Flying", "Munching", "Sleeping"]} />
+			     <Person name = "Ommmmmmmmmmm" age = {1} hobbies = {["Skiing", "Diving", "Snorkling"]} />
+             </div>
+			);
+	}
+}
+
+ReactDOM.render(
+    <App />, document.getElementById('app')
+	)
