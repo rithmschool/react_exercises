c6882a40daf9dbbfe36b1befd7e3c519aa15c39a
Violeta Simovska <vikisimovska27@gmail.com>
2017-12-25 23:06:51 -0800

    working solution

--- 03-events-forms-refs-life-cycle/events/custom_link/src/App.js
+++ 03-events-forms-refs-life-cycle/events/custom_link/src/App.js
@@ -3,15 +3,51 @@ import './App.css';
 import CustomLink from './CustomLink'
 
 class App extends Component {
+   
+  constructor(props){
+    super(props);
+
+    this.state ={
+      url1: "https://www.google.com/",
+      url2: "https://www.google.com/",
+      url3: "https://www.google.com/"
+    }
+
+      this.handleClick = this.handleClick.bind(this);
+  }
+  
+
+    handleClick() {
+      if (this.state.url1 === "https://www.google.com/"){
+         this.setState({
+             url1: "#",
+             url2: "#",
+             url3: "#"
+
+         }
+           
+          )
+      }else{
+        this.setState({
+          url1 : "https://www.google.com/",
+          url2 : "https://www.google.com/",
+          url3 : "https://www.google.com/"
+        }
+        )
+      }
+    }
+    
   render() {
     return (
       <div>
           <h1>Custom Link</h1>
-          <CustomLink url="https://www.google.com/" text="google" />
+          <CustomLink url={this.state.url1} text="google" />
           <br/>
-          <CustomLink url="https://www.google.com/" text="google" />
+          <CustomLink url={this.state.url2} text="google" />
           <br/>
-          <CustomLink url="https://www.google.com/" text="google" />
+          <CustomLink url={this.state.url3} text="google" />
+          <br/>
+          <button onClick={this.handleClick}>Dissable Links</button>
       </div>
     );
   }
--- 03-events-forms-refs-life-cycle/events/custom_link/src/CustomLink.js
+++ 03-events-forms-refs-life-cycle/events/custom_link/src/CustomLink.js
@@ -2,9 +2,18 @@ import React, {Component} from 'react';
 
 class CustomLink extends Component{
 	render(){
+		var link;
+		if(this.props.url !== "#"){
+
+	        link = <a href={this.props.url} target="_blank">{this.props.text}</a>
+		}else{
+			link = <a href={this.props.url}>{this.props.text}</a>
+		}
 		return(
-             <a href={this.props.url} target="_blank">{this.props.text}</a>
-			);
+			<div>
+				{link};
+			</div>
+		);
 	}
 }
 
