a4d78010e9a79e5d75018ebd5c6f779fbb2d33f2
Violeta Simovska <vikisimovska27@gmail.com>
2017-12-25 16:51:55 -0800

    adding vibrant colors

--- 02-props-state-component-architecture/state/randomcolors/src/boxes.js
+++ 02-props-state-component-architecture/state/randomcolors/src/boxes.js
@@ -15,7 +15,6 @@ class Boxes extends Component{
                  <Box 
                     key={n}
                     color= {this.props.colors[Math.floor(Math.random()*24)]}
-                   
                  />
         		);
         })
@@ -28,7 +27,7 @@ class Boxes extends Component{
     }
 }
 Boxes.defaultProps = {
-	colors :["red", "lightred", "darkred", "teal", "yellow", "magenta", "lightblue", "darkblue", "blue", "lightgrey", "grey", "darkgrey", "lightpink", "pink", "darkpink", "lightgreen", "green", "darkgreen", "lightpurple", "purple", "darkpurple", "orange", "amber", "black"],
+	colors :["red", "#A1CAF1", "#00FFFF", "teal", "yellow", "magenta", "lightblue", "darkblue", "blue", "#9966CC", "#FF033E", "#551B8C", "#66B447", "pink", "#7FFFD4", "lightgreen", "green", "darkgreen", "#FDEE00", "purple", "#007FFF", "orange", "#F4C2C2", "black"],
 	numbers : [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
 }
 
