6a3db0d8947ec6b9ebc77fadfa1ce8b1f62b2867
Violeta Simovska <vikisimovska27@gmail.com>
2017-12-25 22:14:49 -0800

    initial setup of the files

---
+++ 03-events-forms-refs-life-cycle/events/custom_link/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import CustomLink from './CustomLink'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Custom Link</h1>
+        </header>
+        
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ 03-events-forms-refs-life-cycle/events/custom_link/src/CustomLink.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class CustomLink extends Component{
+	render(){
+		return();
+	}
+}
+
+default export CustomLink;
