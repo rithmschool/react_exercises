33ab93a8742bc8f555f57400d243d9d7d0490029
Darien Duong <darienduong@gmail.com>
2017-12-27 20:36:06 -0800

    fix header size

--- 02-props-state-component-architecture/props/Props Homework/src/Pokecard.js
+++ 02-props-state-component-architecture/props/Props Homework/src/Pokecard.js
@@ -5,12 +5,11 @@ class Pokecard extends Component {
 	render(){
 		return (
 			<div>
-				<h1></h1>
 				<div className="card">
 				  <img className="card-img-top" src={this.props.image} alt="Pokemon image" />
 				  <div className="card-body">
-				    <h4 className="card-title">{this.props.name}</h4>
-				    <p className="card-text">Type: {this.props.type}</p>
+				    <h1 className="card-title">{this.props.name}</h1>
+				    <h3 className="card-text">Type: {this.props.type}</h3>
 				    <a href="#" className="btn btn-primary">Go somewhere</a>
 				  </div>
 				</div>
