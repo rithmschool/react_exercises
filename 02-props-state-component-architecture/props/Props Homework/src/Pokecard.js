5ea4e2959a80fa33686de8e1f60a9da696c09a15
Darien Duong <darienduong@gmail.com>
2017-12-28 18:16:16 -0800

    add color to h1 tag

--- 02-props-state-component-architecture/props/Props Homework/src/Pokecard.js
+++ 02-props-state-component-architecture/props/Props Homework/src/Pokecard.js
@@ -6,11 +6,10 @@ class Pokecard extends Component {
 		return (
 			<div>
 				<div className="card">
-				  <img className="card-img-top" src={this.props.image} alt="Pokemon image" />
+				  <h1 className="card-title">{this.props.name}</h1>
 				  <div className="card-body">
-				    <h1 className="card-title">{this.props.name}</h1>
+					<img className="card-img-top" src={this.props.image} alt="Pokemon image" />  
 				    <h3 className="card-text">Type: {this.props.type}</h3>
-				    <a href="#" className="btn btn-primary">Go somewhere</a>
 				  </div>
 				</div>
 			</div>
