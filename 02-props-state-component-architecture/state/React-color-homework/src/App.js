517a4491439d2031b9093dd8a32c6de1963502ec
Darien Duong <darienduong@gmail.com>
2018-01-01 22:34:12 -0800

    initial commit

--- 02-props-state-component-architecture/state/React-color-homework/src/App.js
+++ 02-props-state-component-architecture/state/React-color-homework/src/App.js
@@ -1,22 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
 import Colorsquare from './Colorsquare'
-import { Grid, Col, Row } from 'react-bootstrap';
+import { Grid, Row } from 'react-bootstrap';
 
 class App extends Component {
   constructor(props){
     super(props)
-    this.state = {
-      
+    
+    const boxColors = []
+
+    for(var i=0; i<24; i++){
+      boxColors.push({
+        id: i, 
+        color: this.getRandomColor()
+      })
     }
+    
+    this.state = {boxColors}
+
+  }
+
+  getRandomColor(){
+    var randomNumber = Math.floor(Math.random()*this.props.allColors.length)
+    return this.props.allColors[randomNumber]
+  }
+
+  handleChange(idx){
+    const {boxColors} = this.state
+    let newboxColors = boxColors.slice(0, idx)
+    newboxColors.push({id: idx, color: this.getRandomColor()})
+    newboxColors = newboxColors.concat(boxColors.slice(idx + 1))
+    this.setState({
+      boxColors: newboxColors
+    })
   }
 
   render() {
+    let box = this.state.boxColors.map(color => (
+      <Colorsquare 
+        changeColor = {this.handleChange.bind(this, color.id)}
+        color = {color.color}
+        key = {color.id}
+      />
+    ))
     return (
       <div className="App">
         <Grid>
           <Row className = 'show-grid'>
-            {colorSquare}
+            {box}
           </Row>
         </Grid>
       </div>
@@ -25,7 +56,156 @@ class App extends Component {
 }
 
 App.defaultProps = {
-
+  allColors: [
+    'AliceBlue',
+    'AntiqueWhite',
+    'Aqua',
+    'Aquamarine',
+    'Azure',
+    'Beige',
+    'Bisque',
+    'Black',
+    'BlanchedAlmond',
+    'Blue',
+    'BlueViolet',
+    'Brown',
+    'BurlyWood',
+    'CadetBlue',
+    'Chartreuse',
+    'Chocolate',
+    'Coral',
+    'CornflowerBlue',
+    'Cornsilk',
+    'Crimson',
+    'Cyan',
+    'DarkBlue',
+    'DarkCyan',
+    'DarkGoldenRod',
+    'DarkGray',
+    'DarkGrey',
+    'DarkGreen',
+    'DarkKhaki',
+    'DarkMagenta',
+    'DarkOliveGreen',
+    'Darkorange',
+    'DarkOrchid',
+    'DarkRed',
+    'DarkSalmon',
+    'DarkSeaGreen',
+    'DarkSlateBlue',
+    'DarkSlateGray',
+    'DarkSlateGrey',
+    'DarkTurquoise',
+    'DarkViolet',
+    'DeepPink',
+    'DeepSkyBlue',
+    'DimGray',
+    'DimGrey',
+    'DodgerBlue',
+    'FireBrick',
+    'FloralWhite',
+    'ForestGreen',
+    'Fuchsia',
+    'Gainsboro',
+    'GhostWhite',
+    'Gold',
+    'GoldenRod',
+    'Gray',
+    'Grey',
+    'Green',
+    'GreenYellow',
+    'HoneyDew',
+    'HotPink',
+    'IndianRed',
+    'Indigo',
+    'Ivory',
+    'Khaki',
+    'Lavender',
+    'LavenderBlush',
+    'LawnGreen',
+    'LemonChiffon',
+    'LightBlue',
+    'LightCoral',
+    'LightCyan',
+    'LightGoldenRodYellow',
+    'LightGray',
+    'LightGrey',
+    'LightGreen',
+    'LightPink',
+    'LightSalmon',
+    'LightSeaGreen',
+    'LightSkyBlue',
+    'LightSlateGray',
+    'LightSlateGrey',
+    'LightSteelBlue',
+    'LightYellow',
+    'Lime',
+    'LimeGreen',
+    'Linen',
+    'Magenta',
+    'Maroon',
+    'MediumAquaMarine',
+    'MediumBlue',
+    'MediumOrchid',
+    'MediumPurple',
+    'MediumSeaGreen',
+    'MediumSlateBlue',
+    'MediumSpringGreen',
+    'MediumTurquoise',
+    'MediumVioletRed',
+    'MidnightBlue',
+    'MintCream',
+    'MistyRose',
+    'Moccasin',
+    'NavajoWhite',
+    'Navy',
+    'OldLace',
+    'Olive',
+    'OliveDrab',
+    'Orange',
+    'OrangeRed',
+    'Orchid',
+    'PaleGoldenRod',
+    'PaleGreen',
+    'PaleTurquoise',
+    'PaleVioletRed',
+    'PapayaWhip',
+    'PeachPuff',
+    'Peru',
+    'Pink',
+    'Plum',
+    'PowderBlue',
+    'Purple',
+    'Red',
+    'RosyBrown',
+    'RoyalBlue',
+    'SaddleBrown',
+    'Salmon',
+    'SandyBrown',
+    'SeaGreen',
+    'SeaShell',
+    'Sienna',
+    'Silver',
+    'SkyBlue',
+    'SlateBlue',
+    'SlateGray',
+    'SlateGrey',
+    'Snow',
+    'SpringGreen',
+    'SteelBlue',
+    'Tan',
+    'Teal',
+    'Thistle',
+    'Tomato',
+    'Turquoise',
+    'Violet',
+    'Wheat',
+    'White',
+    'WhiteSmoke',
+    'Yellow',
+    'YellowGreen'
+  ]
 }
+
 
 export default App;
