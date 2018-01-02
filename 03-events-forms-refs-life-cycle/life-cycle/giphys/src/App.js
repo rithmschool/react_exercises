021ca19af7389f200806565709c3d907784a5150
Zejian Shen <zesansan@gmail.com>
2018-01-01 17:54:31 -0800

    completed giphy party

---
+++ 03-events-forms-refs-life-cycle/life-cycle/giphys/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import GifList from "./GifList";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <GifList />
+      </div>
+    );
+  }
+}
+
+export default App;
