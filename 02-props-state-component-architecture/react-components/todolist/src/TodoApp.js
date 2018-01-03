43fd995eff4249e2edd5c3cff37520b06810d28a
Trevor Hoy <trevorphoy@gmail.com>
2018-01-02 23:44:02 -0800

    adding todo files

--- 02-props-state-component-architecture/react-components/todolist/src/TodoApp.js
+++ 02-props-state-component-architecture/react-components/todolist/src/TodoApp.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './TodoApp.css';
 
-class App extends Component {
+class TodoApp extends Component {
   render() {
     return (
       <div className="App">
@@ -18,4 +18,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default TodoApp;
--- 02-props-state-component-architecture/react-components/todolist/src/index.js
+++ 02-props-state-component-architecture/react-components/todolist/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import TodoApp from './TodoApp';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<TodoApp />, document.getElementById('root'));
 registerServiceWorker();
