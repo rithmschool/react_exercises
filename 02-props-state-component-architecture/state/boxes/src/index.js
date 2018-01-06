47cb53c173685bb7b97d42377951b82686efbe5d
Trevor Hoy <trevorphoy@gmail.com>
2018-01-06 14:47:14 -0800

    id

--- 02-props-state-component-architecture/state/boxes/src/index.js
+++ 02-props-state-component-architecture/state/boxes/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import TodoApp from './TodoApp';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<TodoApp />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
