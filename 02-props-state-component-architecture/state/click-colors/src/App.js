0017ea4cce636df65439da984601e7b1bbf09d43
Stephen Carrera <stephencarrera@users.noreply.github.com>
2018-01-06 15:46:33 -0800

    click-colors completed

---
+++ 02-props-state-component-architecture/state/click-colors/src/App.js
@@ -0,0 +1,210 @@
+import React, { Component } from "react";
+import Box from "./Box";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    const numBoxes = 24;
+    const boxes = [];
+    for (let i = 0; i < numBoxes; i++) {
+      boxes.push({
+        id: i,
+        backgroundColor: this.getRandomColor()
+      });
+    }
+    this.state = { boxes };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  getRandomColor() {
+    let index = Math.floor(Math.random() * this.props.colors.length);
+    console.log(index);
+    return this.props.colors[index];
+  }
+
+  handleClick(e) {
+    const newBoxes = [...this.state.boxes];
+    newBoxes[e.target.id] = {
+      id: e.target.id,
+      backgroundColor: this.getRandomColor()
+    };
+    this.setState({ boxes: newBoxes });
+  }
+
+  render() {
+    const boxes = this.state.boxes.map(box => {
+      return (
+        <Box
+          key={box.id}
+          id={box.id}
+          backgroundColor={box.backgroundColor}
+          handleClick={this.handleClick}
+        />
+      );
+    });
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to Click Colors!</h1>
+        </header>
+        <p className="App-intro">
+          To get started, click a box to change its color.
+        </p>
+        {boxes}
+      </div>
+    );
+  }
+}
+App.defaultProps = {
+  colors: [
+    "AliceBlue",
+    "AntiqueWhite",
+    "Aqua",
+    "Aquamarine",
+    "Azure",
+    "Beige",
+    "Bisque",
+    "Black",
+    "BlanchedAlmond",
+    "Blue",
+    "BlueViolet",
+    "Brown",
+    "BurlyWood",
+    "CadetBlue",
+    "Chartreuse",
+    "Chocolate",
+    "Coral",
+    "CornflowerBlue",
+    "Cornsilk",
+    "Crimson",
+    "Cyan",
+    "DarkBlue",
+    "DarkCyan",
+    "DarkGoldenRod",
+    "DarkGray",
+    "DarkGrey",
+    "DarkGreen",
+    "DarkKhaki",
+    "DarkMagenta",
+    "DarkOliveGreen",
+    "Darkorange",
+    "DarkOrchid",
+    "DarkRed",
+    "DarkSalmon",
+    "DarkSeaGreen",
+    "DarkSlateBlue",
+    "DarkSlateGray",
+    "DarkSlateGrey",
+    "DarkTurquoise",
+    "DarkViolet",
+    "DeepPink",
+    "DeepSkyBlue",
+    "DimGray",
+    "DimGrey",
+    "DodgerBlue",
+    "FireBrick",
+    "FloralWhite",
+    "ForestGreen",
+    "Fuchsia",
+    "Gainsboro",
+    "GhostWhite",
+    "Gold",
+    "GoldenRod",
+    "Gray",
+    "Grey",
+    "Green",
+    "GreenYellow",
+    "HoneyDew",
+    "HotPink",
+    "IndianRed",
+    "Indigo",
+    "Ivory",
+    "Khaki",
+    "Lavender",
+    "LavenderBlush",
+    "LawnGreen",
+    "LemonChiffon",
+    "LightBlue",
+    "LightCoral",
+    "LightCyan",
+    "LightGoldenRodYellow",
+    "LightGray",
+    "LightGrey",
+    "LightGreen",
+    "LightPink",
+    "LightSalmon",
+    "LightSeaGreen",
+    "LightSkyBlue",
+    "LightSlateGray",
+    "LightSlateGrey",
+    "LightSteelBlue",
+    "LightYellow",
+    "Lime",
+    "LimeGreen",
+    "Linen",
+    "Magenta",
+    "Maroon",
+    "MediumAquaMarine",
+    "MediumBlue",
+    "MediumOrchid",
+    "MediumPurple",
+    "MediumSeaGreen",
+    "MediumSlateBlue",
+    "MediumSpringGreen",
+    "MediumTurquoise",
+    "MediumVioletRed",
+    "MidnightBlue",
+    "MintCream",
+    "MistyRose",
+    "Moccasin",
+    "NavajoWhite",
+    "Navy",
+    "OldLace",
+    "Olive",
+    "OliveDrab",
+    "Orange",
+    "OrangeRed",
+    "Orchid",
+    "PaleGoldenRod",
+    "PaleGreen",
+    "PaleTurquoise",
+    "PaleVioletRed",
+    "PapayaWhip",
+    "PeachPuff",
+    "Peru",
+    "Pink",
+    "Plum",
+    "PowderBlue",
+    "Purple",
+    "Red",
+    "RosyBrown",
+    "RoyalBlue",
+    "SaddleBrown",
+    "Salmon",
+    "SandyBrown",
+    "SeaGreen",
+    "SeaShell",
+    "Sienna",
+    "Silver",
+    "SkyBlue",
+    "SlateBlue",
+    "SlateGray",
+    "SlateGrey",
+    "Snow",
+    "SpringGreen",
+    "SteelBlue",
+    "Tan",
+    "Teal",
+    "Thistle",
+    "Tomato",
+    "Turquoise",
+    "Violet",
+    "Wheat",
+    "White",
+    "WhiteSmoke",
+    "Yellow",
+    "YellowGreen"
+  ]
+};
+export default App;
